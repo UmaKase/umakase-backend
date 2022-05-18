9cf7443a7188a842027a90ea7150458d2256f483
AnjiBoss <nhu.nhu.face@gmail.com>
2022-05-19 00:14:25 +0900

    register

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,9 +11,20 @@ const PORT = process.env.PORT || 5000;
 
 async function main() {
   const app = express();
-  app.use(cors());
-  app.use("/ap1/v1/auth", authRouter);
-  app.use("/ap1/v1/user", userRouter);
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: ["https://hoppscotch.io"],
+    })
+  );
+  app.use("/api/v1/auth", authRouter);
+  app.use("/api/v1/user", userRouter);
+
+  app.get("/", (__, rs) => {
+    rs.json({
+      ok: "kkkk",
+    });
+  });
 
   app.listen(PORT, () => {
     console.log("running on port ", PORT);
