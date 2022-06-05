8dbc1ec6f3dad8d87d90b35324988f882a280922
Anji <nhu.nhu.face@gmail.com>
2022-06-06 06:27:49 +0900

    update database schema for rooms

---
+++ server/src/routes/room.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+const router = express.Router();
+
+
+// router.get("/info/:id",async (req, res) => {
+// 	const id = req.params.id
+// })
+
+
+export {router as roomRouter}
