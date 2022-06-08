85aa63514a239e48e55a60c853759cb4f4716f5f
Anji <nhu.nhu.face@gmail.com>
2022-06-08 16:08:28 +0900

    room routes

--- server/src/routes/room.ts
+++ server/src/routes/room.ts
@@ -1,10 +1,95 @@
-import express from 'express'
+import express from "express";
+import { tokenVerify } from "../middleware/token";
+import { dbclient } from "../server";
 const router = express.Router();
 
+// _GET Room Info
+router.get("/info/:id", tokenVerify, async (req, res) => {
+  const id = req.params.id;
+  const room = await dbclient.room.findFirst({
+    where: { id },
+    include: {
+      foods: {
+        include: {
+          food: true,
+        },
+      },
+      user: {
+        include: {
+          profile: true,
+        },
+      },
+      _count: true,
+    },
+  });
 
-// router.get("/info/:id",async (req, res) => {
-// 	const id = req.params.id
-// })
+  return res.json({
+    ok: true,
+    data: {
+      room,
+    },
+  });
+});
 
+// _POST Create A Room
+// @Body: {string}name, {string[]}roomieNames - username[], {string[]}foodIds.
+router.post("/new", tokenVerify, async (req, res) => {
+  const name = req.body.name;
+  const roomieNames: string[] = req.body.roomie;
+  const foodIds: string[] = req.body.foodIds;
 
-export {router as roomRouter}
+  const creator = await dbclient.profile.findFirst({
+    where: { id: req.profile.id },
+  });
+
+  if (!creator) {
+    return res.json({
+      ok: false,
+      error: {
+        message: "User Not Found! or Authentication error",
+      },
+    });
+  }
+
+  const newRoom = await dbclient.room.create({
+    data: {
+      name,
+      creator: {
+        connect: {
+          id: creator.id,
+        },
+      },
+      foods: {
+        create: foodIds.map((id) => ({
+          food: {
+            connect: {
+              id,
+            },
+          },
+        })),
+      },
+      user: {
+        create: roomieNames.map((username) => ({
+          profile: {
+            connect: { username },
+          },
+        })),
+      },
+    },
+    include: {
+      foods: {
+        include: { food: true },
+      },
+      user: {
+        include: { profile: true },
+      },
+    },
+  });
+
+  return res.json({
+    ok: true,
+    data: { newRoom },
+  });
+});
+
+export { router as roomRouter };
