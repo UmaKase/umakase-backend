37fbb82d344001f22c9d3005e022137842eff700
Anji <nhu.nhu.face@gmail.com>
2022-06-29 17:27:32 +0900

    add foods into room

--- server/src/routes/library.ts
+++ server/src/routes/library.ts
@@ -1,9 +1,31 @@
 import { PrismaClient } from "@prisma/client";
-import express from "express";
+import express, { Response } from "express";
+import { tokenVerify } from "../middleware/token";
+import { ResponseObject } from "../utils/ResponseController";
 const router = express.Router();
 
 const prisma = new PrismaClient();
 
-router.get("/", async (req, res) => {});
+// Add Food To Room
+router.post("/room", tokenVerify, async (req, res) => {
+  const foodIds: string[] = req.body.foodIds;
+  const roomId = req.body.roomId;
+
+  const updatedRoom = await prisma.room.update({
+    where: { id: roomId },
+    data: {
+      foods: {
+        connect: foodIds.map((id) => ({
+          roomId_foodId: {
+            foodId: id,
+            roomId,
+          },
+        })),
+      },
+    },
+  });
+
+  return new ResponseObject(res, true, 200, "Success", updatedRoom).send();
+});
 
 export { router as libraryRouter };
