323d306936b59742798ccebca9c288614aaf5633
anjiboss <nhu.nhu.face@gmail.com>
2022-10-03 05:08:12 +0900

    update roomes

--- src/routes/room.ts
+++ src/routes/room.ts
@@ -3,6 +3,7 @@ import { PrismaClient, Profile, Room } from "@prisma/client";
 import express from "express";
 import { tokenVerify } from "@middleware/token";
 import { ResponseObject } from "@utils/ResponseController";
+import { Log } from "@utils/Log";
 
 /*
  ************************************
@@ -12,6 +13,7 @@ import { ResponseObject } from "@utils/ResponseController";
 
 const router = express.Router();
 const prisma = new PrismaClient();
+const log = new Log();
 
 // _GET Room Info
 router.get("/info/:id", tokenVerify, async (req, res) => {
@@ -128,6 +130,116 @@ router.post("/new", tokenVerify, async (req, res) => {
   return new ResponseObject(res, true, 200, "created successfully", {
     newRoom,
   });
+});
+
+router.put("/update/:roomId", tokenVerify, async (req, res) => {
+  const user = await prisma.profile.findFirst({
+    where: {
+      id: req.profile.id,
+    },
+  });
+
+  if (!user) {
+    return new ResponseObject(res, false, 401, "Authorization Error");
+  }
+
+  const roomId = req.params.roomId;
+
+  const name = req.body.name;
+  try {
+    await prisma.room.update({
+      where: {
+        id: roomId,
+      },
+      data: {
+        name,
+      },
+    });
+    return new ResponseObject(res, true, 200, "Room updated");
+  } catch (error) {
+    log.error(error);
+    return new ResponseObject(
+      res,
+      false,
+      400,
+      "Room not found or already deleted"
+    );
+  }
+});
+
+router.post("/event", tokenVerify, async (req, res) => {
+  const event = req.body.event;
+  const roomId = req.body.roomId;
+
+  if (!helper.isRoomEvent(event)) {
+    return new ResponseObject(
+      res,
+      false,
+      400,
+      "No Event or Invalid Room Event"
+    );
+  }
+
+  const room = await prisma.room.findFirst({
+    where: { id: roomId },
+    include: {
+      user: {
+        where: {
+          profileId: req.profile.id,
+        },
+      },
+    },
+  });
+
+  if (!room) {
+    return new ResponseObject(res, false, 400, "Room not found");
+  }
+
+  let [result, error, data] = [false, "", undefined];
+  switch (event) {
+    case "add-member":
+      const newRoomies = req.body.newRoomies;
+      if (!newRoomies) {
+        break;
+      }
+      [result, error, data] = await helper.addRoomMember(roomId, newRoomies);
+
+      return new ResponseObject(res, result, result ? 200 : 400, error, data);
+    case "remove-member":
+      const removeRoomies = req.body.removeRoomies;
+      if (!removeRoomies) {
+        break;
+      }
+      [result, error, data] = await helper.removeRoomMember(
+        roomId,
+        removeRoomies
+      );
+
+      return new ResponseObject(res, result, result ? 200 : 400, error, data);
+    case "update-food":
+      break;
+
+    default:
+      // code not expected to goes here
+      return new ResponseObject(
+        res,
+        false,
+        400,
+        "No Event or Invalid Room Event"
+      );
+  }
+
+  log.error(
+    `\n\tRoom Event: ${event} | Payload: ${JSON.stringify(req.body)}`,
+    `Error: ${error}`,
+    data
+  );
+  return new ResponseObject(
+    res,
+    false,
+    400,
+    "Input not enough, please report to admin"
+  );
 });
 
 export { router as roomRouter };
