4d70ad04ca6ab706151ecc062fb353585807b963
anjiboss <nhu.nhu.face@gmail.com>
2023-03-02 19:56:26 +0900

    add unwrap func

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -16,3 +16,8 @@ declare global {
  * - update-food - when food is not update correctly and user try to reload. send this event
  */
 export type RoomEvent = "add-member" | "remove-member" | "update-food";
+
+// try catch wrapper
+declare global {
+  function unwrap<T>(promise: Promise<T>): Promise<[T?, any?]>;
+}
