8722df04d0aab7f031c8e1d4f60aa14eaba684b5
anjiboss <nhu.nhu.face@gmail.com>
2023-03-08 17:37:02 +0900

    try catch wrapper done and add jsdoc

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -19,5 +19,18 @@ export type RoomEvent = "add-member" | "remove-member" | "update-food";
 
 // try catch wrapper
 declare global {
+  /**
+   * Try Catch Wrapper
+   * @param promise Promise or async function
+   * @returns [awaited promise, error]
+   * @example
+   * ```ts
+   * const [result, error] = await unwrap(someAsyncFunction());
+   * if (error) {
+   *  // handle error
+   * }
+   * result; // result is type of T
+   * ```
+   */
   function unwrap<T>(promise: Promise<T>): Promise<[T?, any?]>;
 }
